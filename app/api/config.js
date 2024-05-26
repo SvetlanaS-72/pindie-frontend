6afb9a097f58d346a1da4ea263212cc11222a11d
SvetlanaS-72 <146142217+SvetlanaS-72@users.noreply.github.com>
2024-05-26 20:15:04 +0300

    feat:chang URL back

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://student-svetf.nomoredomainswork.ru/api/'
+export const BASE_URL = 'http://student-svets.nomoredomainswork.ru/api/'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
